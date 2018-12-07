5eb9addfd591447501abddc0e29c343850c5c398
a.bragagnolo <alberto.bragagnolo@pd.infn.it>
2018-12-07 14:24:41 +0100

    Create submitAll.sh

---
+++ ntuple/2017/submitAll.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd BdJpsiKstar; bash bsub.sh; cd ..;
+cd BdKxMuMu; bash bsub.sh; cd ..;
+cd BsJpsiPhi; bash bsub.sh; cd ..;
+cd BsJpsiPhiDGamma0; bash bsub.sh; cd ..;
+cd BuJpsiK; bash bsub.sh; cd ..;
