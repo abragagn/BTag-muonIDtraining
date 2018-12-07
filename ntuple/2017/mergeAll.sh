d93a83f9c4783c629cff2a8c3339e9856eca25bb
a.bragagnolo <alberto.bragagnolo@pd.infn.it>
2018-12-07 14:25:05 +0100

    Create mergeAll.sh

---
+++ ntuple/2017/mergeAll.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd BdJpsiKstar; bash merge.sh; cd ..;
+cd BdKxMuMu; bash merge.sh; cd ..;
+cd BsJpsiPhi; bash merge.sh; cd ..;
+cd BsJpsiPhiDGamma0; bash merge.sh; cd ..;
+cd BuJpsiK; bash merge.sh; cd ..;
