0511ca7df785aca616314116a9d119397d860a6e
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2019-02-21 17:31:40 +0100

    Cleaning, add possibility to train on all eta range

--- training/2017/trainingGlobal2017.py
+++ training/2017/trainingGlobal2017.py
@@ -20,22 +20,22 @@ from keras.callbacks import ModelCheckpoint
 ##### FUNCTIONS
 
 def getKerasModel(inputDim, modelName, layerSize = 100, nLayers = 5, dropValue = 0.5):
-	model = Sequential()
-	model.add(Dense(layerSize, activation='relu', kernel_initializer='normal', input_dim=inputDim))
-	model.add(Dropout(dropValue))
+    model = Sequential()
+    model.add(Dense(layerSize, activation='relu', kernel_initializer='normal', input_dim=inputDim))
+    model.add(Dropout(dropValue))
 
-	for i in range(1, nLayers):
-		model.add(Dense(layerSize, activation='relu', kernel_initializer='normal'))
-		model.add(Dropout(dropValue))
+    for i in range(1, nLayers):
+        model.add(Dense(layerSize, activation='relu', kernel_initializer='normal'))
+        model.add(Dropout(dropValue))
 
-	model.add(Dense(2, activation='softmax'))
+    model.add(Dense(2, activation='softmax'))
 
-	sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-	model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-	
-	model.save(modelName)
-	model.summary()
-	return
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    
+    model.save(modelName)
+    model.summary()
+    return
 
 ##### MAIN
 
@@ -43,130 +43,129 @@ def getKerasModel(inputDim, modelName, layerSize = 100, nLayers = 5, dropValue =
 TMVA.Tools.Instance()
 TMVA.PyMethodBase.PyInitialize()
 
+# Parameters
 region = sys.argv[1]
 var1 = sys.argv[2]
 var2 = sys.argv[3]
 
 DNNFLAG = True
 BDTFLAG = True
-DEBUG 	= False
+DEBUG   = False
 
-if region != 'Barrel' and region != 'Endcap':
-	print 'Invalid argument region'
-	sys.exit()
+if region != 'Barrel' and region != 'Endcap' region != 'Full':
+    print 'Invalid argument region'
+    sys.exit()
 
 if var1 != 'wIP' and var1 != 'woIP':
-	print 'Invalid argument var1'
-	sys.exit()
+    print 'Invalid argument var1'
+    sys.exit()
 
 if var2 != 'wIso' and var2 != 'woIso':
-	print 'Invalid argument var2'
-	sys.exit()
+    print 'Invalid argument var2'
+    sys.exit()
 
-
-outputName = 'TMVAGlobal' + region + '2017' + var1 + var2 + '.root'
-
-
+outputName = 'TMVAMuonID' + region + '2017' + var1 + var2 + '.root'
 output = TFile.Open(outputName, 'RECREATE')
 
+# Factory
 factory = TMVA.Factory('TMVAClassification', output,
                        '!V:!Silent:Color:DrawProgressBar:AnalysisType=Classification')
 
 # Load data
-if DEBUG:
-	data = TFile.Open('bankBsJpsiPhiDGamma017.root')
-	tree = data.Get('PDsecondTree')
-else:
-	dataBs = TFile.Open('bankBsJpsiPhi17.root')
-	dataBsD0 = TFile.Open('bankBsJpsiPhiDGamma017.root')
-	dataBu = TFile.Open('bankBuJpsiK17.root')
-	dataBd = TFile.Open('bankBdJpsiKx17.root')
-	dataBdNR = TFile.Open('bankBdKxMuMu17.root')
+dataBs = TFile.Open('bankBsJpsiPhi17.root')
+dataBsD0 = TFile.Open('bankBsJpsiPhiDGamma017.root')
+dataBu = TFile.Open('bankBuJpsiK17.root')
+dataBd = TFile.Open('bankBdJpsiKx17.root')
+dataBdNR = TFile.Open('bankBdKxMuMu17.root')
 
-	treeBs = dataBs.Get('PDsecondTree')
-	treeBsD0 = dataBsD0.Get('PDsecondTree')
-	treeBu = dataBu.Get('PDsecondTree')
-	treeBd = dataBd.Get('PDsecondTree')
-	treeBdNR = dataBdNR.Get('PDsecondTree')
+treeBs = dataBs.Get('PDsecondTree')
+treeBsD0 = dataBsD0.Get('PDsecondTree')
+treeBu = dataBu.Get('PDsecondTree')
+treeBd = dataBd.Get('PDsecondTree')
+treeBdNR = dataBdNR.Get('PDsecondTree')
 
 dataloader = TMVA.DataLoader('dataset')
 
 # add variables
 varList = [
-	('muoPt', 'F')
-	,('abs(muoEta)', 'F')
-        ,('muoSegmComp', 'F')
-        ,('muoChi2LM', 'F')
-        ,('muoChi2LP', 'F')
-        ,('muoGlbTrackTailProb', 'F')
-        ,('muoIValFrac', 'F')
-        ,('muoLWH', 'I')
-        ,('muoTrkKink', 'F')
-        ,('muoGlbKinkFinderLOG', 'F')
-        ,('muoTimeAtIpInOutErr', 'F')
-        ,('muoOuterChi2', 'F')
-        ,('muoInnerChi2', 'F')
-        ,('muoTrkRelChi2', 'F')
-        ,('muoVMuonHitComb', 'I')
-        ,('muoGlbDeltaEtaPhi', 'F')
-        ,('muoStaRelChi2', 'F')
-        ,('muoTimeAtIpInOut', 'F')
-        ,('muoValPixHits', 'I')
-        ,('muoNTrkVHits', 'I')
-        ,('muoGNchi2', 'F')
-        ,('muoVMuHits', 'I')
-        ,('muoNumMatches', 'F')
-        ,('muoQprod', 'I')
-	]
+    ('muoPt', 'F')
+    ,('abs(muoEta)', 'F')
+    ,('muoSegmComp', 'F')
+    ,('muoChi2LM', 'F')
+    ,('muoChi2LP', 'F')
+    ,('muoGlbTrackTailProb', 'F')
+    ,('muoIValFrac', 'F')
+    ,('muoLWH', 'I')
+    ,('muoTrkKink', 'F')
+    ,('muoGlbKinkFinderLOG', 'F')
+    ,('muoTimeAtIpInOutErr', 'F')
+    ,('muoOuterChi2', 'F')
+    ,('muoInnerChi2', 'F')
+    ,('muoTrkRelChi2', 'F')
+    ,('muoVMuonHitComb', 'I')
+    ,('muoGlbDeltaEtaPhi', 'F')
+    ,('muoStaRelChi2', 'F')
+    ,('muoTimeAtIpInOut', 'F')
+    ,('muoValPixHits', 'I')
+    ,('muoNTrkVHits', 'I')
+    ,('muoGNchi2', 'F')
+    ,('muoVMuHits', 'I')
+    ,('muoNumMatches', 'F')
+    ,('muoQprod', 'I')
+    ]
 
 
 if var1 == 'wIP':
-	varList.append(('trkDxy/trkExy', 'F'))
-	varList.append(('trkDz/trkEz', 'F'))
+    varList.append(('trkDxy/trkExy', 'F'))
+    varList.append(('trkDz/trkEz', 'F'))
 if var2 == 'wIso':
-	varList.append(('muoPFiso', 'F'))
+    varList.append(('muoPFiso', 'F'))
 
 nVars = 0
 for var in varList:
-	dataloader.AddVariable( var[0], var[1] )
-	nVars += 1
+    dataloader.AddVariable( var[0], var[1] )
+    nVars += 1
 
 dataloader.AddSpectator( 'muoEvt', 'I' )
 
 # prepare dataloader
 
-mycutgen = 'muoPt>=2&&(muoEvt%10)<8'
+mycutgen = 'muoPt>=2'
+#mycutgen += '&&(muoEvt%10)<8'
 
 if region == 'Barrel':
-	mycutgen += '&&abs(muoEta)<1.2'
+    mycutgen += '&&abs(muoEta)<1.2'
 elif region == 'Endcap':
-	mycutgen += '&&abs(muoEta)>=1.2'
+    mycutgen += '&&abs(muoEta)>=1.2'
 
 mycuts = mycutgen + '&&abs(muoLund)==13&&muoAncestor>=0'
 mycutb = mycutgen + '&&abs(muoLund)!=13'
 
 if DEBUG:
-	dataloader.AddSignalTree(tree, 1.0)
-	dataloader.AddBackgroundTree(tree, 1.0)
+    dataloader.AddSignalTree(tree, 1.0)
+    dataloader.AddBackgroundTree(tree, 1.0)
 else:
-	dataloader.AddSignalTree(treeBs, 0.2)
-	dataloader.AddSignalTree(treeBsD0, 0.2)
-	dataloader.AddSignalTree(treeBu, 0.2)
-	dataloader.AddSignalTree(treeBd, 0.2)
-	dataloader.AddSignalTree(treeBdNR, 0.2)
+    dataloader.AddSignalTree(treeBs, 0.2)
+    dataloader.AddSignalTree(treeBsD0, 0.2)
+    dataloader.AddSignalTree(treeBu, 0.2)
+    dataloader.AddSignalTree(treeBd, 0.2)
+    dataloader.AddSignalTree(treeBdNR, 0.2)
 
-	dataloader.AddBackgroundTree(treeBs, 1.0)
-	dataloader.AddBackgroundTree(treeBsD0, 1.0)
-	dataloader.AddBackgroundTree(treeBu, 1.0)
-	dataloader.AddBackgroundTree(treeBd, 1.0)
-	dataloader.AddBackgroundTree(treeBdNR, 1.0)
+    dataloader.AddBackgroundTree(treeBs, 1.0)
+    dataloader.AddBackgroundTree(treeBsD0, 1.0)
+    dataloader.AddBackgroundTree(treeBu, 1.0)
+    dataloader.AddBackgroundTree(treeBd, 1.0)
+    dataloader.AddBackgroundTree(treeBdNR, 1.0)
 
 if region == 'Barrel':
-	nBkg = '49800'
-	nSgn = '500000'
+    nBkg = '49800'
+    nSgn = '500000'
 elif region == 'Endcap':
-	nBkg = '111750'
-	nSgn = '1120000'
+    nBkg = '111750'
+    nSgn = '1120000'
+elif region == 'Full':
+    nBkg = '161550'
+    nSgn = '1620000'
 
 dataloaderOpt = 'nTrain_Signal=' + nSgn + ':nTrain_Background=' + nBkg + ':nTest_Signal=' + nSgn + ':nTest_Background=' + nBkg
 dataloaderOpt += ':SplitMode=Random:NormMode=NumEvents:!V'
@@ -174,42 +173,37 @@ dataloaderOpt += ':SplitMode=Random:NormMode=NumEvents:!V'
 dataloader.PrepareTrainingAndTestTree(TCut(mycuts), TCut(mycutb), dataloaderOpt)
 
 if DNNFLAG:
-	# Define model
-	modelName = 'model' + region + '17' + var1 + var2 + '.h5'
-	dropValue = 0.5
-	layerSize = 100
-	nLayers = 5
+    # Define model
+    modelName = 'model' + region + '17' + var1 + var2 + '.h5'
+    dropValue = 0.5
+    layerSize = 100
+    nLayers = 5
 
-	if DEBUG:
-		dropValue = 0.0
-		nLayers = 1
+    getKerasModel(nVars, modelName, layerSize, nLayers, dropValue)
 
-	getKerasModel(nVars, modelName, layerSize, nLayers, dropValue)
+    # Book methods
+    dnnOptions = '!H:!V:FilenameModel=' + modelName + ':NumEpochs=100:TriesEarlyStopping=20:BatchSize=128'
 
-	# Book methods
-	dnnOptions = '!H:!V:FilenameModel=' + modelName + ':NumEpochs=1000:TriesEarlyStopping=50:BatchSize=128'
+    iVar = 0
+    preprocessingOptions = ':VarTransform=N'
+    preprocessingOptions += ',G('
+    for var in varList:
+        if var[0] == 'muoQprod':
+            iVar += 1
+            continue
+        preprocessingOptions += '_V' + str(iVar) + '_' + ','
+        iVar += 1
+    preprocessingOptions = preprocessingOptions[:-1]
+    preprocessingOptions +=  '),N'
+    
+    dnnName = 'DNNMuonID' + region + '2017' + var1 + var2
 
-	iVar = 0
-	preprocessingOptions = ':VarTransform=N'
-	preprocessingOptions += ',G('
-	for var in varList:
-		if var[0] == 'muoQprod':
-			iVar += 1
-			continue
-		preprocessingOptions += '_V' + str(iVar) + '_' + ','
-		iVar += 1
-	preprocessingOptions = preprocessingOptions[:-1]
-	preprocessingOptions +=  '),N'
-	
-	dnnName = 'DNNMuonID' + region + '2017' + var1 + var2
-
-	factory.BookMethod(dataloader, TMVA.Types.kPyKeras, dnnName, dnnOptions + preprocessingOptions)
-
-	
+    factory.BookMethod(dataloader, TMVA.Types.kPyKeras, dnnName, dnnOptions + preprocessingOptions)
+    
 if BDTFLAG:
-	bdtOptions = '!H:!V:UseBaggedBoost:BaggedSampleFraction=0.6:NTrees=600:MaxDepth=8:nCuts=100:MinNodeSize=1.0%:BoostType=RealAdaBoost:AdaBoostBeta=0.3:SigToBkgFraction=2:DoBoostMonitor=True'
-	bdtName = 'BDTMuonID' + region + '2017' + var1 + var2
-	factory.BookMethod(dataloader, TMVA.Types.kBDT, bdtName, bdtOptions)
+    bdtOptions = '!H:!V:UseBaggedBoost:BaggedSampleFraction=0.6:NTrees=600:MaxDepth=8:nCuts=100:MinNodeSize=1.0%:BoostType=RealAdaBoost:AdaBoostBeta=0.3:SigToBkgFraction=2:DoBoostMonitor=True'
+    bdtName = 'BDTMuonID' + region + '2017' + var1 + var2
+    factory.BookMethod(dataloader, TMVA.Types.kBDT, bdtName, bdtOptions)
 
 # Run training, test and evaluation
 factory.TrainAllMethods()
